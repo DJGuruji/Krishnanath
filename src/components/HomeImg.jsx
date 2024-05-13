49443d06d7a97e6c7c854d9ad6c2f125fe59f3c6
DJGuruji <nath93266@gmail.com>
2024-05-13 20:15:47 +0530

    img size cahnged

--- src/components/HomeImg.jsx
+++ src/components/HomeImg.jsx
@@ -5,7 +5,7 @@ const HomeImg = () => {
   return (
     <>
       <div className='w-full flex justify-center items-center'>
-        <img src={img} alt="face" className='border-2 border-yellow-300 w-44 h-58 rounded-full md:mr-28 lg:mr-28 xl:mr-28' />
+        <img src={img} alt="face" className='border-2 border-yellow-300 w-44 h-58 rounded-sm md:mr-28 lg:mr-28 xl:mr-28' />
 
       </div>
     </>
