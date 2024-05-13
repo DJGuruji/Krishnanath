0aa3e30f63c65d48b3f9fbb0b80c3607ca018bf0
DJGuruji <nath93266@gmail.com>
2024-05-13 21:04:58 +0530

    img position changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
     <div className="h-screen bg-black">
       <div className="md:flex lg:flex xl:flex md:justify-around lg:justify-around xl:justify-around">
       
-        <div className="pt-10 flex flex-col justify-center items-center ">
+        <div className="pt-10 flex  justify-center items-center pl-5 ">
           <HomeImg></HomeImg>
         </div>
-        <div className="pt-4 flex flex-col justify-center ">
+        <div className="pb-4 flex flex-col justify-center items-center">
           <Discription></Discription>
         </div>
       </div>
