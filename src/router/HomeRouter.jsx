658dbe2fc8000ba5d13a31f4349e9d3f73a79d47
DJGuruji <nath93266@gmail.com>
2024-10-04 21:47:09 +0530

    z-index issue of router solved

--- src/router/HomeRouter.jsx
+++ src/router/HomeRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -7,7 +7,7 @@ import Projects from "../pages/Projects";
 import NotFound from "../pages/PNF";
 import Contact from "../pages/Contact";
 import { FaHome } from "react-icons/fa";
-import { FaFileAlt } from "react-icons/fa"
+import { FaFileAlt } from "react-icons/fa";
 import { MdOutlineMiscellaneousServices } from "react-icons/md";
 import { FaDiagramProject } from "react-icons/fa6";
 import { IoMdContact } from "react-icons/io";
@@ -15,13 +15,10 @@ import { IoMdContact } from "react-icons/io";
 const HomeRouter = () => {
   const [dropdown, setDropdown] = useState(false);
   const navigate = useNavigate();
- 
 
   useEffect(() => {
-    
-    if (window.location.pathname !== '/') {
-     
-      navigate('/');
+    if (window.location.pathname !== "/") {
+      navigate("/");
     }
   }, []);
 
@@ -35,38 +32,43 @@ const HomeRouter = () => {
 
   return (
     <>
-      <nav className="bg-black h-16 border-b-2 border-slate-500">
+      <nav className="bg-black h-16 border-b-2 border-slate-500 relative">
         <div className="flex justify-between w-screen items-center">
-          <h1 className="text-yellow-300 text-2xl font-bold ml-3 font-slab ">
-          <Link to="/" onClick={handleItemClick}>
-          Krishnanath.S
-          </Link>
+          <h1 className="text-yellow-300 text-2xl font-bold ml-3 font-slab">
+            <Link to="/" onClick={handleItemClick}>
+              Krishnanath.S
+            </Link>
           </h1>
           <div className="w-1/2"></div>
           <div className="md:flex hidden">
             <ul className="flex text-lg">
               <li className="mt-2 p-2 text-white font-light hover:font-bold">
-                <Link to="/" onClick={handleItemClick} className="flex"><FaHome className="m-1"/>
+                <Link to="/" onClick={handleItemClick} className="flex">
+                  <FaHome className="m-1" />
                   Home
                 </Link>
               </li>
               <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <Link to="/about" onClick={handleItemClick} className="flex"><FaFileAlt className="m-1"/>
+                <Link to="/about" onClick={handleItemClick} className="flex">
+                  <FaFileAlt className="m-1" />
                   About
                 </Link>
               </li>
               <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <Link to="/service" onClick={handleItemClick} className="flex"><MdOutlineMiscellaneousServices className="m-1"/>
+                <Link to="/service" onClick={handleItemClick} className="flex">
+                  <MdOutlineMiscellaneousServices className="m-1" />
                   Service
                 </Link>
               </li>
               <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <Link to="/projects" onClick={handleItemClick} className="flex"><FaDiagramProject className="m-1"/>
+                <Link to="/projects" onClick={handleItemClick} className="flex">
+                  <FaDiagramProject className="m-1" />
                   Projects
                 </Link>
               </li>
               <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <Link to="/contact" onClick={handleItemClick} className="flex"><IoMdContact className="m-1 text-xl"/>
+                <Link to="/contact" onClick={handleItemClick} className="flex">
+                  <IoMdContact className="m-1 text-xl" />
                   Contact
                 </Link>
               </li>
@@ -79,33 +81,40 @@ const HomeRouter = () => {
             >
               <div className="w-8 h-1 bg-white m-1 rounded-lg"></div>
               <div className="w-7 h-1 bg-white ml-2 rounded-lg"></div>
-              {/* <div className="w-6 h-1 bg-white mt-1 ml-3"></div> */}
             </button>
             {dropdown && (
-              <div className="absolute mt-5 rounded-md shadow-lg bg-black right-0 border-2 border-yellow-300">
+              <div
+                className="absolute mt-5 rounded-md shadow-lg bg-black right-0 border-2 border-yellow-300 z-50"
+                style={{ zIndex: 50 }} // Add high z-index
+              >
                 <ul className="flex flex-col text-lg mr-4">
                   <li className="mt-2 p-2 text-white font-light hover:font-bold text-center ">
-                    <Link to="/" onClick={handleItemClick} className="flex"><FaHome className="m-1"/>
+                    <Link to="/" onClick={handleItemClick} className="flex">
+                      <FaHome className="m-1" />
                       Home
                     </Link>
                   </li>
                   <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <Link to="/about" onClick={handleItemClick} className="flex"><FaFileAlt className="m-1"/>
+                    <Link to="/about" onClick={handleItemClick} className="flex">
+                      <FaFileAlt className="m-1" />
                       About
                     </Link>
                   </li>
                   <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <Link to="/service" onClick={handleItemClick} className="flex"><MdOutlineMiscellaneousServices className="m-1"/>
+                    <Link to="/service" onClick={handleItemClick} className="flex">
+                      <MdOutlineMiscellaneousServices className="m-1" />
                       Service
                     </Link>
                   </li>
                   <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <Link to="/projects" onClick={handleItemClick} className="flex"><FaDiagramProject className="m-1"/>
+                    <Link to="/projects" onClick={handleItemClick} className="flex">
+                      <FaDiagramProject className="m-1" />
                       Projects
                     </Link>
                   </li>
                   <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <Link to="/contact" onClick={handleItemClick} className="flex"><IoMdContact className="m-1 text-xl"/>
+                    <Link to="/contact" onClick={handleItemClick} className="flex">
+                      <IoMdContact className="m-1 text-xl" />
                       Contact
                     </Link>
                   </li>
