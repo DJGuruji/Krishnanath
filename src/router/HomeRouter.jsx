0cdf7ee5b7737096374064d1c2185a73341003a6
DJGuruji <nath93266@gmail.com>
2024-10-05 16:55:05 +0530

    hash rout

--- src/router/HomeRouter.jsx
+++ src/router/HomeRouter.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, Routes, Route, useNavigate } from "react-router-dom"; // Import NavLink
+import { NavLink, Routes, Route, HashRouter, useNavigate } from "react-router-dom"; 
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Service from "../pages/Service";
@@ -31,148 +31,82 @@ const HomeRouter = () => {
   };
 
   return (
-    <>
-      <nav className="bg-black h-16 border-b-2 border-slate-500 relative">
-        <div className="flex justify-between w-screen items-center">
-          <h1 className="text-yellow-300 text-2xl font-bold ml-3 font-slab">
-            <NavLink to="/" onClick={handleItemClick} className="text-yellow-300">
-              Krishnanath.S
-            </NavLink>
-          </h1>
-          <div className="w-1/2"></div>
-          <div className="md:flex hidden">
-            <ul className="flex text-lg">
-              <li className="mt-2 p-2 text-white font-light hover:font-bold">
-                <NavLink 
-                  to="/" 
-                  onClick={handleItemClick} 
-                  className={({ isActive }) => (isActive ? 'flex underline font-bold ' : 'flex')}
-                >
-                  <FaHome className="m-1" />
-                  Home
-                </NavLink>
-              </li>
-              <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <NavLink 
-                  to="/about" 
-                  onClick={handleItemClick} 
-                  className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                >
-                  <FaFileAlt className="m-1" />
-                  About
-                </NavLink>
-              </li>
-              <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <NavLink 
-                  to="/service" 
-                  onClick={handleItemClick} 
-                  className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                >
-                  <MdOutlineMiscellaneousServices className="m-1" />
-                  Service
-                </NavLink>
-              </li>
-              <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <NavLink 
-                  to="/projects" 
-                  onClick={handleItemClick} 
-                  className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                >
-                  <FaDiagramProject className="m-1" />
-                  Projects
-                </NavLink>
-              </li>
-              <li className="p-2 mt-2 text-white font-light hover:font-bold">
-                <NavLink 
-                  to="/contact" 
-                  onClick={handleItemClick} 
-                  className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                >
-                  <IoMdContact className="m-1 text-xl" />
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
+    <HashRouter>
+      <>
+        <nav className="bg-black h-16 border-b-2 border-slate-500 relative">
+          <div className="flex justify-between w-screen items-center">
+            <h1 className="text-yellow-300 text-2xl font-bold ml-3 font-slab">
+              <NavLink to="/" onClick={handleItemClick} className="text-yellow-300">
+                Krishnanath.S
+              </NavLink>
+            </h1>
+            <div className="w-1/2"></div>
+            <div className="md:flex hidden">
+              <ul className="flex text-lg">
+                <li className="mt-2 p-2 text-white font-light hover:font-bold">
+                  <NavLink 
+                    to="/" 
+                    onClick={handleItemClick} 
+                    className={({ isActive }) => (isActive ? 'flex underline font-bold ' : 'flex')}
+                  >
+                    <FaHome className="m-1" />
+                    Home
+                  </NavLink>
+                </li>
+                <li className="p-2 mt-2 text-white font-light hover:font-bold">
+                  <NavLink 
+                    to="/about" 
+                    onClick={handleItemClick} 
+                    className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
+                  >
+                    <FaFileAlt className="m-1" />
+                    About
+                  </NavLink>
+                </li>
+                <li className="p-2 mt-2 text-white font-light hover:font-bold">
+                  <NavLink 
+                    to="/service" 
+                    onClick={handleItemClick} 
+                    className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
+                  >
+                    <MdOutlineMiscellaneousServices className="m-1" />
+                    Service
+                  </NavLink>
+                </li>
+                <li className="p-2 mt-2 text-white font-light hover:font-bold">
+                  <NavLink 
+                    to="/projects" 
+                    onClick={handleItemClick} 
+                    className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
+                  >
+                    <FaDiagramProject className="m-1" />
+                    Projects
+                  </NavLink>
+                </li>
+                <li className="p-2 mt-2 text-white font-light hover:font-bold">
+                  <NavLink 
+                    to="/contact" 
+                    onClick={handleItemClick} 
+                    className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
+                  >
+                    <IoMdContact className="m-1 text-xl" />
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <button
-              className="text-white mt-4 relative md:hidden mr-2"
-              onClick={handleDropdown}
-            >
-              <div className="w-8 h-1 bg-white m-1 rounded-lg"></div>
-              <div className="w-7 h-1 bg-white ml-2 rounded-lg"></div>
-            </button>
-            {dropdown && (
-              <div
-                className="absolute mt-5 rounded-md shadow-lg bg-black right-0 border-2 border-yellow-300 z-50"
-                style={{ zIndex: 50 }} 
-              >
-                <ul className="flex flex-col text-lg mr-4">
-                  <li className="mt-2 p-2 text-white font-light hover:font-bold text-center">
-                    <NavLink 
-                      to="/" 
-                      onClick={handleItemClick} 
-                      className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                    >
-                      <FaHome className="m-1" />
-                      Home
-                    </NavLink>
-                  </li>
-                  <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <NavLink 
-                      to="/about" 
-                      onClick={handleItemClick} 
-                      className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                    >
-                      <FaFileAlt className="m-1" />
-                      About
-                    </NavLink>
-                  </li>
-                  <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <NavLink 
-                      to="/service" 
-                      onClick={handleItemClick} 
-                      className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                    >
-                      <MdOutlineMiscellaneousServices className="m-1" />
-                      Service
-                    </NavLink>
-                  </li>
-                  <li className="p-2 mt-2 text-white font-light hover:font-bold text-center">
-                    <NavLink 
-                      to="/projects" 
-                      onClick={handleItemClick} 
-                      className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                    >
-                      <FaDiagramProject className="m-1" />
-                      Projects
-                    </NavLink>
-                  </li>
-                  <li className="p-2 mt-2 mr-10 text-white font-light hover:font-bold text-center">
-                    <NavLink 
-                      to="/contact" 
-                      onClick={handleItemClick} 
-                      className={({ isActive }) => (isActive ? 'flex underline font-bold' : 'flex')}
-                    >
-                      <IoMdContact className="m-1 text-xl" />
-                      Contact
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </>
+    </HashRouter>
   );
 };
 
