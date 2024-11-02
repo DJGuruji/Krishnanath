4166c260388b7c3b7d68f14c1f53a5171183f9d7
DJGuruji <nath93266@gmail.com>
2024-11-02 11:43:49 +0530

    updated projects

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Personal Web
 
 
+## Project setup
 
- <div className='text-white flex flex-col items-center'>
-      <div className='flex p-3'>
-        <h1 className='text-xl  text-yellow-300'>Degree: </h1>
-        <h1 className='text-xl  '> Btech Computer Science And Engineering(2021-2025)</h1>
-      </div>
-      <div className='flex p-3 '>
-        <h1 className='text-xl text-yellow-300'>College: </h1>
-        <h1 className=' text-xl '> Vidya Academy Of Science And Technology Technical Campus, Kilimanoor</h1>
-      </div>
-      <div className='flex p-3'>
-        <h1 className=' text-xl text-yellow-300'>Language: </h1>
-        <h1 className=' text-xl  '> malayalam, English</h1>
-      </div>
-      <div className='flex flex-col border-2 border-yellow-300 m-2 '>
-        <h1 className=' text-xl text-yellow-300 text-center'>Skills </h1>
-        <p className=' text-xl '> MERN stack, Django, MySQL,PSQL,Oracle,HTML,CSS,JavaScript,AWS,GIT, GitHub</p>
-      </div>
+```bash
+https://github.com/DJGuruji/Krishnanath.git
+cd Krishnanath
+npm install
+npm run dev
+```
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -51,26 +51,41 @@ const ProjectsPage = () => {
       githubLink: 'https://github.com/DJGuruji/MedicalAppointment',
       productionLink: 'nil'
     },
-    {
-      name: 'Password Generator',
-      description: 'This project is a Password Generator web application built with React and styled using Tailwind CSS, designed to provide users with a simple yet powerful tool for creating secure passwords. The application allows users to customize the password generation process by specifying the length and choosing whether to include uppercase letters, lowercase letters, numbers, and symbols. With a dynamic and responsive interface, the password is generated in real-time based on the selected criteria, and a loading state provides visual feedback during the process. Once the password is generated, users can easily copy it to their clipboard with a single click, with a confirmation message indicating the success of the action. The sleek and modern design, powered by Tailwind CSS, ensures a clean and intuitive user experience, making this tool both functional and visually appealing across various devices',
-      status: 'completed',
-      githubLink: 'https://github.com/DJGuruji/PasswordGenerator',
-      productionLink: 'https://djguruji.github.io/PasswordGenerator'
-    },
+
     {
       name: 'Entrance React',
       description: 'Embark on your online entrance exam journey where knowledge meets opportunity. Our platform, meticulously crafted using React, offers a seamless experience to aspiring candidates. With each question, delve into a world of choices, as you select from four meticulously curated options. But beware, time is of the essence, with a countdown timer ticking away 1.5 hours of opportunity. Once the clock runs out, the site gracefully halts, revealing the culmination of your efforts. Witness your mastery unfold as your marks in physics, chemistry, and mathematics materialize before your eyes. It is not just an exam it is a gateway to your future.',
       status: 'completed',
       githubLink: 'https://github.com/DJGuruji/EntranceReact',
-      productionLink: 'nil'
+      productionLink: 'https://keammock.deno.dev'
     },
     {
       name: 'Complaint Management System',
       description: 'Our complaint management system is built with React and Tailwind CSS, offering separate interfaces for users and administrators. Users navigate a streamlined portal for lodging complaints and tracking their status, while administrators access a dedicated platform for efficient management and resolution. With user-friendly design and intuitive functionality, our system ensures seamless communication and swift action, enhancing the experience for both users and administrators.',
-      status: 'in progress',
+      status: 'completed',
       githubLink: 'https://github.com/DJGuruji/cmsfront',
-      productionLink: 'https://djguruji.github.io/cmsfront'
+      productionLink: 'https:cmsreact.deno.dev'
+    },
+    {
+      name: 'Complaint Portal',
+      description: 'Enter the realm of efficient complaint resolution with our online portal, meticulously constructed using HTML5, Tailwind CSS, and JavaScript. Designed to streamline the complaint handling process, our platform offers comprehensive features tailored for administrators, including settings management and branch addition functionalities. Administrators can effortlessly navigate through options such as adding branches, configuring settings, and more, ensuring smooth operation and customization of the portal. With intuitive design and seamless functionality, our platform empowers administrators to efficiently manage complaints and deliver prompt resolutions. Experience the pinnacle of complaint management efficiency with our online portal, built to simplify administrative tasks and enhance user satisfaction.',
+      status: 'completed',
+      githubLink: 'https://github.com/DJGuruji/Complaint-portal',
+      productionLink: 'https://djguruji.github.io/Complaint-portal/dist/'
+    },
+    {
+      name: 'Bookmark',
+      description: 'Developed a dynamic bookmarking application using React, allowing users to save and manage their favorite links effectively. The app displays each bookmark with the date it was added and provides options for users to update existing links. With an intuitive interface, users can easily organize and edit their bookmarks, creating a streamlined experience for tracking and accessing frequently visited sites',
+      status: 'completed',
+      githubLink: 'https://github.com/DJGuruji/Bookmark',
+      productionLink: 'https://gbookmark.deno.dev'
+    },
+    {
+      name: 'Password Generator',
+      description: 'Developed a customizable password generator using React, allowing users to create strong, secure passwords based on selected criteria. The application enables users to tailor their password strength by choosing to include uppercase and lowercase letters, numbers, and special characters. The intuitive interface empowers users to set their security preferences easily, enhancing password robustness for a variety of use cases',
+      status: 'completed',
+      githubLink: 'https://github.com/DJGuruji/PasswordGenerator',
+      productionLink: 'https://passwordgenerator.deno.dev'
     },
     {
       name: 'Ecommerce',
@@ -86,13 +101,7 @@ const ProjectsPage = () => {
       githubLink: 'https://github.com/DJGuruji/Calculator',
       productionLink: 'https://djguruji.github.io/Calculator'
     },
-    {
-      name: 'Complaint Portal',
-      description: 'Enter the realm of efficient complaint resolution with our online portal, meticulously constructed using HTML5, Tailwind CSS, and JavaScript. Designed to streamline the complaint handling process, our platform offers comprehensive features tailored for administrators, including settings management and branch addition functionalities. Administrators can effortlessly navigate through options such as adding branches, configuring settings, and more, ensuring smooth operation and customization of the portal. With intuitive design and seamless functionality, our platform empowers administrators to efficiently manage complaints and deliver prompt resolutions. Experience the pinnacle of complaint management efficiency with our online portal, built to simplify administrative tasks and enhance user satisfaction.',
-      status: 'completed',
-      githubLink: 'https://github.com/DJGuruji/Complaint-portal',
-      productionLink: 'https://djguruji.github.io/Complaint-portal/dist/'
-    },
+   
     {
       name: 'Calculato-JS',
       description: 'In my first project, I have ventured into the realm of web development by creating a simple yet functional calculator using HTML, CSS, and JavaScript. Through this endeavor, I have delved into the fundamentals of front-end design and programming, honing my skills in crafting user interfaces and implementing logic to perform basic arithmetic operations. From addition to division, my calculator offers a seamless experience for users to perform calculations effortlessly. This project marks the beginning of my journey in web development, providing me with valuable hands-on experience and paving the way for future endeavors in the field',
