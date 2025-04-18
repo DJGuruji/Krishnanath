30e479337da044266611026881d9a04d56c92293
DJGuruji <nath93266@gmail.com>
2025-04-18 12:46:03 +0530

    updated style

--- src/components/Discription.jsx
+++ src/components/Discription.jsx
@@ -32,12 +32,12 @@ const ImagLink = ({ src, alt, href, isHovered, handleHover }) => {
 
 const Discription = () => {
   const [isHovered, setIsHovered] = useState(false);
-  
+
   const handleHover = () => {
     setIsHovered(!isHovered);
   };
   const navigate = useNavigate();
-  
+
   return (
     <section className="py-12 pb-20 md:py-20 px-6 md:px-10 bg-gradient-to-b from-black to-zinc-900">
       <div className="max-w-4xl mx-auto">
@@ -45,11 +45,11 @@ const Discription = () => {
           <h2 className="font-sans text-xl font-medium text-yellow-400">
             Hello!
           </h2>
-          
+
           <h1 className="text-4xl md:text-5xl font-bold text-white font-playfair">
             I am <span className="text-yellow-400">Krishnanath S</span>
           </h1>
-          
+
           <div className="flex flex-wrap text-lg text-zinc-400 font-light">
             <span className="mr-2">Software Engineer</span>
             <span className="mr-2 text-yellow-400">|</span>
@@ -57,28 +57,31 @@ const Discription = () => {
             <span className="mr-2 text-yellow-400">|</span>
             <span>Villupuram, Tamil Nadu</span>
           </div>
-          
+
           <p className="text-xl text-zinc-300 leading-relaxed mt-8 max-w-3xl">
-            I'm a <span className="text-yellow-400 font-medium">Full Stack Web Developer</span> with extensive experience in building and developing websites using the MERN stack, Django, FastAPI and Next.js.
+            I'm a{" "}
+            <span className="text-yellow-400 font-medium">
+              Full Stack Web Developer
+            </span>{" "}
+            with extensive experience in building and developing websites using
+            the MERN stack, Django, FastAPI and Next.js.
           </p>
-          
-         
-            <button 
-        
+
+          <div className="mt-8 mb-8 hidden md:flex">
+            <button
               className="btn-primary"
               onClick={() => navigate("/projects")}
             >
               View My Work
             </button>
-            
+
             <button
-          
               className="ml-4 px-6 py-2 bg-transparent text-yellow-400 font-semibold rounded-sm border border-yellow-500 hover:bg-yellow-500/10 transition-colors duration-300"
               onClick={() => navigate("/contact")}
-              >
+            >
               Contact Me
             </button>
-     
+          </div>
         </div>
       </div>
     </section>
